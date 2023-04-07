59b28d112c0d3fbe19c1320ff0c4e28b28cbf774
yaredow <yaredyilma11@gmail.com>
2023-04-07 17:19:02 +0300

    excerisce on datatype

--- Fundamentals-part-1/script.js
+++ Fundamentals-part-1/script.js
@@ -1,3 +1,11 @@
-let js = "Amazing";
-alert("JavaScript if FUN!");
 console.log(56 + 45 + 56);
+let firstName = "yared";
+console.log(firstName);
+
+// Excercis on Variables
+let country = "Ethiopia",
+  continent = "Africa",
+  population = "120 million";
+console.log(`${country}, ${continent}, ${population}`);
+
+// Excercise on Datatype
