c0e716ef72fbfbecfab901ea3f09e28f30c13a2a
yaredow <yaredyilma11@gmail.com>
2023-04-07 17:41:53 +0300

    legacy bug in JS

--- Fundamentals-part-1/script.js
+++ Fundamentals-part-1/script.js
@@ -1,11 +1,22 @@
-console.log(56 + 45 + 56);
-let firstName = "yared";
-console.log(firstName);
+// console.log(56 + 45 + 56);
+// let firstName = "yared";
+// console.log(firstName);
 
-// Excercis on Variables
-let country = "Ethiopia",
-  continent = "Africa",
-  population = "120 million";
-console.log(`${country}, ${continent}, ${population}`);
+// // Excercis on Variables
+// let country = "Ethiopia",
+//   continent = "Africa",
+//   population = "120 million";
+// console.log(`${country}, ${continent}, ${population}`);
 
 // Excercise on Datatype
+// let javaScriptIsFun = true;
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof "Yared");
+// console.log(typeof 23);
+
+// javaScriptIsFun = "YES";
+// console.log(typeof javaScriptIsFun);
+
+year = 1991;
+console.log(typeof year);
