462d408233861eec77ecd9a516518e5fcfda29aa
yaredow <yaredyilma11@gmail.com>
2023-04-10 10:57:59 +0300

    loose and strict equality signs

--- Fundamentals-part-1/script.js
+++ Fundamentals-part-1/script.js
@@ -29,9 +29,95 @@
 // const firstName = "Yared";
 // const lastName = "Yilma";
 // console.log(firstName + " " + lastName);
-let x = 10 + 5;
-x += 10;
-x *= 10;
-x++;
-x--;
-console.log(x);
+// let x = 10 + 5;
+// x += 10;
+// x *= 10;
+// x++;
+// x--;
+// console.log(x);
+// var result = 5 + 4 * 3 > 10 - 2;
+// console.log(result);
+// coding exercise 1
+
+// strings
+// const firstName = "Yared";
+// const job = "engineer";
+// const birthYear = 1996;
+// const yearNow = "2023";
+
+// // const yared =
+// //   "I'm" +
+// //   " " +
+// //   firstName +
+// //   ", a" +
+// //   " " +
+// //   (yearNow - birthYear) +
+// //   " " +
+// //   "years old" +
+// //   " " +
+// //   job +
+// //   ".";
+// // console.log(yared);
+// const newYared = `I'm ${firstName}, a ${yearNow - birthYear} years old ${job}.`;
+// console.log(`christopher nolan is
+// the greates directior of all time
+// I swear to god yo`);
+// coditon and decision making in JavaScript
+// const age = 19;
+// const isOldEnough = age >= 20;
+// if (isOldEnough) {
+//   console.log("you are allowed");
+// } else {
+//   yearsLeft = 20 - age;
+//   console.log(`you are not allowed. Please wait another ${yearsLeft} years.`);
+// Exercise #3
+
+// let markHeight = 1.85,
+//   markWeight = 78;
+// let jhonHeight = 1.95,
+//   johnWeight = 92;
+// MarkBMI = markWeight / markHeight ** 2;
+// johnBMI = johnWeight / jhonHeight ** 2;
+// console.log(MarkBMI);
+// console.log(johnBMI);
+// markHigherBMI = MarkBMI > johnBMI;
+
+// if (markHigherBMI) {
+//   console.log(
+//     `Mark's BMI, ${MarkBMI}, is greater than John's BMI, ${johnBMI}.`
+//   );
+// } else {
+//   console.log(`Mark's BMI, ${MarkBMI}, is less than John's BMI, ${johnBMI}.`);
+// }
+
+// // Type coercison in JavaScript
+// const inputYear = "1991";
+// console.log(Number(inputYear) + 4);
+// // console.log(String(23));
+// const num = 123;
+// const str = "123";
+// console.log(num + str);
+// let n = "1" - 1;
+// n = n - 1;
+// console.log(n);
+// const money = 0;
+// console.log(typeof money);
+// if (money) {
+//   console.log("Spend as much as you want");
+// } else {
+//   console.log("You shouldn't spend much");
+// }
+
+// let height;
+// if (height) {
+//   console.log("Yay! height is defined!");
+// } else {
+//   console.log("Height is not defined");
+// }
+
+const age = 18;
+if (age === "18") {
+  console.log("You are an adult");
+} else {
+  console.log("You are a piece of shit");
+}
