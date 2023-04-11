14581c584290879d1a7c7def2efeef44d1aee5fe
yaredow <yaredyilma11@gmail.com>
2023-04-11 17:03:18 +0300

    fruit juice function

---
+++ Fundamentals-part-2/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// const private = 123;
+// function fruitJuice(Apple, Orange) {
+//   console.log(Apple, Orange);
+//   const jucie = `Juice with ${Apple} Apple and ${Orange} orange.`;
+//   return jucie;
+// }
+// const orangeJuice = fruitJuice(0, 4);
+// console.log(orangeJuice);
+// const appleJuice = fruitJuice(4, 0);
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitJuice(3, 5);
+// console.log(appleOrangeJuice);
