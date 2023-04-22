834e6f371dad1f6bb9484879a06a2ac72a931635
yaredow <yaredyilma11@gmail.com>
2023-04-22 16:47:21 +0300

    add an event listener to listen the escape key and closed the modal

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -4,8 +4,27 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnClosModal = document.querySelector('.close-modal');
-// console.log(btnShowModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnShowModal.length; i++) {
-  console.log(btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener('click', openModal);
 }
+
+btnClosModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// close the modal when the ESC key is pressed
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
