271bea338d25ceb8735df7633a53001665897c74
yaredow <yaredyilma11@gmail.com>
2023-04-22 15:24:42 +0300

    change the name of folder

---
+++ 06-Modal/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnClosModal = document.querySelector('.close-modal');
+// console.log(btnShowModal);
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+}
