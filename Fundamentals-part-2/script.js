3bf860ed2aecdf4c37df5fab6b03e98120478918
yaredow <yaredyilma11@gmail.com>
2023-04-22 14:47:31 +0300

    refactor the code by using functions

--- Fundamentals-part-2/script.js
+++ Fundamentals-part-2/script.js
@@ -1,207 +1,221 @@
-'use strict';
-// const private = 123;
-// function fruitJuice(Apple, Orange) {
-//   console.log(Apple, Orange);
-//   const jucie = `Juice with ${Apple} Apple and ${Orange} orange.`;
-//   return jucie;
-// }
-// const orangeJuice = fruitJuice(0, 4);
-// console.log(orangeJuice);
-// const appleJuice = fruitJuice(4, 0);
-// console.log(appleJuice);
-// const appleOrangeJuice = fruitJuice(3, 5);
-// console.log(appleOrangeJuice);
+// 'use strict';
+// // const private = 123;
+// // function fruitJuice(Apple, Orange) {
+// //   console.log(Apple, Orange);
+// //   const jucie = `Juice with ${Apple} Apple and ${Orange} orange.`;
+// //   return jucie;
+// // }
+// // const orangeJuice = fruitJuice(0, 4);
+// // console.log(orangeJuice);
+// // const appleJuice = fruitJuice(4, 0);
+// // console.log(appleJuice);
+// // const appleOrangeJuice = fruitJuice(3, 5);
+// // console.log(appleOrangeJuice);
 
-// function declaration
-// function calcAge1(birthYear) {
-//   return 2023 - birthYear;
-// }
-// const myAge1 = calcAge1(1996);
+// // function declaration
+// // function calcAge1(birthYear) {
+// //   return 2023 - birthYear;
+// // }
+// // const myAge1 = calcAge1(1996);
 
-// // function expression
+// // // function expression
 
-// const calcAge2 = function (birthYear) {
-//   return 2023 - birthYear;
-// };
-// const myAge2 = calcAge2(1996);
-// console.log(myAge1, myAge2);
-// Arrow function
+// // const calcAge2 = function (birthYear) {
+// //   return 2023 - birthYear;
+// // };
+// // const myAge2 = calcAge2(1996);
+// // console.log(myAge1, myAge2);
+// // Arrow function
 
-// const add = (a, b) => a + b;
+// // const add = (a, b) => a + b;
 
-// const addition = add(3, 4);
-// console.log(addition);
-// const fruitPieces = function (fruit) {
-//   return fruit * 4;
-// };
+// // const addition = add(3, 4);
+// // console.log(addition);
+// // const fruitPieces = function (fruit) {
+// //   return fruit * 4;
+// // };
 
-// const fruitprocessor = function (apples, oranges) {
-//   const applePieces = fruitPieces(apples);
-//   const orangePieces = fruitPieces(oranges);
-//   const juice = `juice with ${applePieces} apples and ${orangePieces} oranges`;
-//   return juice;
-// };
-// console.log(fruitprocessor(3, 2));
-// const color = ["red", "blue", "green", "yellow"];
-// const myFavColor = color[color.length - 1];
-// console.log(` my favorite color is ${myFavColor}`);
+// // const fruitprocessor = function (apples, oranges) {
+// //   const applePieces = fruitPieces(apples);
+// //   const orangePieces = fruitPieces(oranges);
+// //   const juice = `juice with ${applePieces} apples and ${orangePieces} oranges`;
+// //   return juice;
+// // };
+// // console.log(fruitprocessor(3, 2));
+// // const color = ["red", "blue", "green", "yellow"];
+// // const myFavColor = color[color.length - 1];
+// // console.log(` my favorite color is ${myFavColor}`);
 
-// const bill = 250;
-// console.log(
-//   ` bill is ${bill} and the tip is ${
-//     bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100
-//   }`
-// );
+// // const bill = 250;
+// // console.log(
+// //   ` bill is ${bill} and the tip is ${
+// //     bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100
+// //   }`
+// // );
 
-// Objects
+// // Objects
 
-// const Yared = {
-//   firstName: "Yared",
-//   lastName: "Yilma",
-//   birthYear: 1996,
-//   job: "Software developer  ",
-//   friends: ["Yonas", "Nahom", "Tom"],
-//   hasDriversLicense: true,
-//   calcAge: function () {
-//     this.age = 2023 - this.birthYear;
-//     return this.age;
-//   },
-//   getSummary: function () {
-//     return `${this.firstName} is a ${this.age} years old ${
-//       this.job
-//     }, and he has ${this.hasDriversLicense ? "a" : "no"} drivers license`;
-//   },
-// };
+// // const Yared = {
+// //   firstName: "Yared",
+// //   lastName: "Yilma",
+// //   birthYear: 1996,
+// //   job: "Software developer  ",
+// //   friends: ["Yonas", "Nahom", "Tom"],
+// //   hasDriversLicense: true,
+// //   calcAge: function () {
+// //     this.age = 2023 - this.birthYear;
+// //     return this.age;
+// //   },
+// //   getSummary: function () {
+// //     return `${this.firstName} is a ${this.age} years old ${
+// //       this.job
+// //     }, and he has ${this.hasDriversLicense ? "a" : "no"} drivers license`;
+// //   },
+// // };
 
-// console.log(Yared["firstName"]);
+// // console.log(Yared["firstName"]);
 
-// const nameKey = "Name";
-// console.log(Yared["first" + nameKey]);
-// console.log(Yared["last" + nameKey]);
-// const interestedIn = prompt(
-//   "what do you want to know about Yared? choose between firstName, lastName, age, job, or friends?"
-// );
+// // const nameKey = "Name";
+// // console.log(Yared["first" + nameKey]);
+// // console.log(Yared["last" + nameKey]);
+// // const interestedIn = prompt(
+// //   "what do you want to know about Yared? choose between firstName, lastName, age, job, or friends?"
+// // );
 
-// if (Yared[interestedIn]) {
-//   console.log(Yared[interestedIn]);
-// } else {
-//   console.log("Wrong choice.");
-// }
-
-// Yared.location = "Ethiopia";
-// Yared["Twitter"] = "@yaredow";
-
-// console.log(
-//   `${Yared.firstName} has ${Yared.friends.length} friends, and his best friend is ${Yared.friends[1]}`
-// );
-// console.log(Yared.calcAge());
-
-// console.log(Yared.getSummary());
-
-// const mark = {
-//   firstName: "Mark",
-//   lastName: "Miller",
-//   height: 1.69,
-//   weight: 78,
-//   calcBMI: function () {
-//     this.bmi = this.weight / this.height ** 2;
-//     return this.bmi;
-//   },
-// };
-
-// const john = {
-//   firstName: "John",
-//   lastName: "Smith",
-//   height: 1.95,
-//   weight: 92,
-//   calcBMI: function () {
-//     this.bmi = this.weight / this.height ** 2;
-//     return this.bmi;
-//   },
-// };
-
-// const markBMI = mark.calcBMI();
-// const johnBMI = john.calcBMI();
-// if (markBMI > johnBMI) {
-//   console.log(
-//     `${mark.firstName}'s BMI (${markBMI}) is higher than ${john.firstName}'s BMI (${johnBMI}) `
-//   );
-// } else {
-//   console.log(
-//     `${john.firstName}'s BMI (${johnBMI}) is higher than ${mark.firstName}'s BMI ${markBMI} `
-//   );
-// }
-
-// const yared = ["Yared", "Yilma", 2023 - 1996, ["Nati", "Nahom", "Sam"]];
-// const type = [];
-
-// for (let i = 0; i < yared.length; i++) {
-//   console.log(yared[i], typeof [i]);
-//   type[i] = typeof yared[i];
-// }
-
-// console.log(type);
-
-// const years = [1991, 1993, 2001, 1996, 1965];
-// const ages = [];
-// for (let i = 0; i < years.length; i++) {
-//   ages.push(2023 - years[i]);
-// }
-// console.log(ages);
-
-// const yared = ["Yared", "Yilma", 2023 - 1996, ["Nati", "Nahom", "Sam"]];
-// // for (let i = 0; i < yared.length; i++) {
-// //   if (typeof yared[i] !== "string") continue;
-// //   console.log(yared[i], typeof yared[i]);
+// // if (Yared[interestedIn]) {
+// //   console.log(Yared[interestedIn]);
+// // } else {
+// //   console.log("Wrong choice.");
 // // }
 
-// for (let i = yared.length - 1; i >= 0; i--) {
-//   console.log(i, yared[i]);
-// }
+// // Yared.location = "Ethiopia";
+// // Yared["Twitter"] = "@yaredow";
 
-// for (let exercise = 1; exercise < 4; exercise++) {
-//   console.log(`------ Starting excercise ${exercise}`);
-//   for (let rep = 1; rep < 6; rep++) {
-//     console.log(`Exercise ${exercise}: Lifting weight repitation ${rep}`);
+// // console.log(
+// //   `${Yared.firstName} has ${Yared.friends.length} friends, and his best friend is ${Yared.friends[1]}`
+// // );
+// // console.log(Yared.calcAge());
+
+// // console.log(Yared.getSummary());
+
+// // const mark = {
+// //   firstName: "Mark",
+// //   lastName: "Miller",
+// //   height: 1.69,
+// //   weight: 78,
+// //   calcBMI: function () {
+// //     this.bmi = this.weight / this.height ** 2;
+// //     return this.bmi;
+// //   },
+// // };
+
+// // const john = {
+// //   firstName: "John",
+// //   lastName: "Smith",
+// //   height: 1.95,
+// //   weight: 92,
+// //   calcBMI: function () {
+// //     this.bmi = this.weight / this.height ** 2;
+// //     return this.bmi;
+// //   },
+// // };
+
+// // const markBMI = mark.calcBMI();
+// // const johnBMI = john.calcBMI();
+// // if (markBMI > johnBMI) {
+// //   console.log(
+// //     `${mark.firstName}'s BMI (${markBMI}) is higher than ${john.firstName}'s BMI (${johnBMI}) `
+// //   );
+// // } else {
+// //   console.log(
+// //     `${john.firstName}'s BMI (${johnBMI}) is higher than ${mark.firstName}'s BMI ${markBMI} `
+// //   );
+// // }
+
+// // const yared = ["Yared", "Yilma", 2023 - 1996, ["Nati", "Nahom", "Sam"]];
+// // const type = [];
+
+// // for (let i = 0; i < yared.length; i++) {
+// //   console.log(yared[i], typeof [i]);
+// //   type[i] = typeof yared[i];
+// // }
+
+// // console.log(type);
+
+// // const years = [1991, 1993, 2001, 1996, 1965];
+// // const ages = [];
+// // for (let i = 0; i < years.length; i++) {
+// //   ages.push(2023 - years[i]);
+// // }
+// // console.log(ages);
+
+// // const yared = ["Yared", "Yilma", 2023 - 1996, ["Nati", "Nahom", "Sam"]];
+// // // for (let i = 0; i < yared.length; i++) {
+// // //   if (typeof yared[i] !== "string") continue;
+// // //   console.log(yared[i], typeof yared[i]);
+// // // }
+
+// // for (let i = yared.length - 1; i >= 0; i--) {
+// //   console.log(i, yared[i]);
+// // }
+
+// // for (let exercise = 1; exercise < 4; exercise++) {
+// //   console.log(`------ Starting excercise ${exercise}`);
+// //   for (let rep = 1; rep < 6; rep++) {
+// //     console.log(`Exercise ${exercise}: Lifting weight repitation ${rep}`);
+// //   }
+// // }
+
+// // let dice = Math.trunc(Math.random() * 6) + 1;
+// // while (dice != 6) {
+// //   console.log(`you rolled ${dice}`);
+// //   dice = Math.trunc(Math.random() * 6) + 1;
+// //   if (dice === 6) {
+// //     console.log("The loop is about to end...");
+// //   }
+// // }
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// for (let i = 0; i <= bills.length - 1; i++) {
+//   const calcTip = function (bill) {
+//     if (bill >= 50 && bill <= 300) {
+//       return (bill * 15) / 100;
+//     } else {
+//       return (bill * 20) / 100;
+//     }
+//   };
+
+//   tips.push(calcTip(bills[i]));
+//   totals.push(bills[i] + tips[i]);
+// }
+// const calcAvg = function (arr) {
+//   let sum = 0;
+//   for (let k = 0; k <= arr.length - 1; k++) {
+//     sum += arr[k];
 //   }
-// }
+//   return sum / arr.length;
+// };
 
-// let dice = Math.trunc(Math.random() * 6) + 1;
-// while (dice != 6) {
-//   console.log(`you rolled ${dice}`);
-//   dice = Math.trunc(Math.random() * 6) + 1;
-//   if (dice === 6) {
-//     console.log("The loop is about to end...");
-//   }
-// }
+// const avgValue = calcAvg(totals);
+// console.log(avgValue);
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// //checking prittier single quote
 
-for (let i = 0; i <= bills.length - 1; i++) {
-  const calcTip = function (bill) {
-    if (bill >= 50 && bill <= 300) {
-      return (bill * 15) / 100;
-    } else {
-      return (bill * 20) / 100;
-    }
-  };
+// console.log('I am single quote');
 
-  tips.push(calcTip(bills[i]));
-  totals.push(bills[i] + tips[i]);
-}
-const calcAvg = function (arr) {
-  let sum = 0;
-  for (let k = 0; k <= arr.length - 1; k++) {
-    sum += arr[k];
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// temperature amplitude = Max temo 0 Min temp
+
+const CalcTempAmp = function (temps) {
+  let Max = temps[0];
+  let Min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    
+
   }
-  return sum / arr.length;
 };
-
-const avgValue = calcAvg(totals);
-console.log(avgValue);
-
-//checking prittier single quote
-
-console.log('I am single quote');
